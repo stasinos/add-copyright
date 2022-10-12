40811bebc05ae7d0b651530fa8b673367a4cc559
Stasinos Konstantopoulos <stasinos@konstant.gr>
2022-10-12 10:30:43 +0300

    Support for different syntaxes

--- addcopyright.sh
+++ addcopyright.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
-## Solution found as part of this stackoverflow discussion: https://stackoverflow.com/questions/151677/tool-for-adding-license-headers-to-source-files
+declare -A applyRE
+declare -A addpre
+declare -A addpost
 
-for x in $*; do  
-head -$COPYRIGHTLEN $x | diff copyright.txt - || ( ( cat copyright.txt; echo; cat $x) > /tmp/file;  
-mv /tmp/file $x )  
-done 
+addpre[c]='/*'
+applyRE[c]='s/^/ * /'
+addpost[c]=' */'
+
+applyRE[shell]='s/^/# /'
+applyRE[matlab]='s/^/% /'
+applyRE[lua]='s/^/-- /'
+
+if [[ x$1 == x ]]; then
+    echo 'No style given, applying default "c"' >&2
+    STYLE="c"
+elif [[ -v "applyRE[$1]" ]] ; then
+    STYLE=$1
+else
+    echo "Unknown style"
+    exit 1
+fi
+
+tmpcopyright=$(mktemp /tmp/addcopyright.XXXXXX)
+
+if [[ -v "addpre[$STYLE]" ]] ; then echo "${addpre[$STYLE]}" > $tmpcopyright ; fi
+sed "${applyRE[$STYLE]}" < copyright.txt >> $tmpcopyright
+if [[ -v "addpost[$STYLE]" ]] ; then echo "${addpost[$STYLE]}" >> $tmpcopyright; fi
+COPYRIGHTLEN=$(wc -l $tmpcopyright)
+
+## Solution found as part of this stackoverflow discussion: https://stackoverflow.com/questions/151677/tool-for-adding-license-headers-to-source-files
+tmpsrcfile=$(mktemp /tmp/addcopyright.XXXXXX)
+for x in *; do
+    echo "doing $x"
+    head -$COPYRIGHTLEN $x | diff copyright.txt - || ( (cat $tmpcopyright; echo; cat $x) > $tmpsrcfile; mv $tmpsrcfile $x )
+done
+
+rm -f $tmpcopyright $tmpsrcfile
+
+exit 0
